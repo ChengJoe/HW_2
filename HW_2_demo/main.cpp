68d6a6cef219a2cab856cc6a56dedc35763677fc
ChengJoe <joe900509@gmail.com>
2021-03-29 08:53:01 -0700

    both button and display can work

--- HW_2_demo/main.cpp
+++ HW_2_demo/main.cpp
@@ -2,45 +2,105 @@
 #include "uLCD_4DGL.h"
 using namespace std::chrono;
 
-uLCD_4DGL uLCD(D1, D0, D2); // UART4 tx, UART4 rx, reset pin;
-DigitalOut button_1_send(D4);
-// DigitalIn button_1_receive(D5);
-InterruptIn button_1_receive(D5);
-DigitalOut myled(LED1);
+uLCD_4DGL uLCD(D1, D0, D2);
+// DigitalOut button_1_send(D4);
+InterruptIn button_up_count(D5);
+InterruptIn button_down_count(D6);
+InterruptIn button_confirm(D9);
 
-Timer t;
-Thread thread_1;
-
-int test_number = 0;
-
-EventQueue queue(32 * EVENTS_EVENT_SIZE);
+Timer debounce;
+Thread thread_uLCD(osPriorityNormal);
+EventQueue queue_uLCD(32 * EVENTS_EVENT_SIZE);
+int test_number[3] = {500, 868, 1000};
+int i = 0;
+bool IsConfirm = false;
 
 void Display_to_uLCD(){
-    uLCD.cls();
-    uLCD.locate(2,2);
-    uLCD.printf("Current number is %d",test_number);
-    ThisThread::sleep_for(2s);
+        uLCD.text_width(4); //4X size text
+        uLCD.text_height(4);
+        if(!IsConfirm)
+            uLCD.color(BLUE);
+        else 
+            uLCD.color(GREEN);
+        uLCD.locate(0,2);
+        uLCD.printf("%4d\n",test_number[i]);
 }
 
-void toggle(){
-    if(duration_cast<milliseconds>(t.elapsed_time()).count()> 500){
-        myled = !myled;
-        test_number++;
-        t.reset();
-        queue.call(Display_to_uLCD);
-    }        
+void Up_count(){
+    if (duration_cast<milliseconds>(debounce.elapsed_time()).count() > 600){
+        if (i < 2 && !IsConfirm){
+            i += 1;
+            queue_uLCD.call(Display_to_uLCD);
+            debounce.reset();
+        }
+    }
+}
+
+void Down_count(){
+    if (duration_cast<milliseconds>(debounce.elapsed_time()).count() > 600){
+        if (i > 0 && !IsConfirm){
+            i -= 1;
+            queue_uLCD.call(Display_to_uLCD);
+            debounce.reset();
+        }
+    }
+}
+
+void Output_wave(){
+
+}
+
+void Confirm(){
+    if (duration_cast<milliseconds>(debounce.elapsed_time()).count() > 600){
+        IsConfirm = !IsConfirm;
+        queue_uLCD.call(Display_to_uLCD);
+        debounce.reset();
+    }    
 }
 
 int main()
 {
-    thread_1.start(callback(&queue, &EventQueue::dispatch_forever));
-    t.start();
-    button_1_send = 1;
-    button_1_receive.rise(&toggle);
-    myled = 1;
-    while (1) { 
-        printf("test_number is %d\n",test_number);
-        ThisThread::sleep_for(1s);
-    }
-    // uLCD.printf("\nByebye uLCD World\n"); //Default Green on black text
+    uLCD.color(RED);
+    uLCD.locate(2,2);
+    uLCD.printf("Current freq. is\n");
+    uLCD.text_width(4); //4X size text
+    uLCD.text_height(4);
+    uLCD.color(BLUE);
+    uLCD.locate(0,2);
+    uLCD.printf("%4d\n",test_number[i]);
+    debounce.start();
+    thread_uLCD.start(callback(&queue_uLCD, &EventQueue::dispatch_forever));
+    button_confirm.rise(queue_uLCD.event(Confirm));
+    button_up_count.rise(queue_uLCD.event(Up_count));
+    button_down_count.rise(queue_uLCD.event(Down_count));
 }
+
+// #include "mbed.h"
+// #include "uLCD_4DGL.h"
+
+// //InterruptIn down(D7);
+// //InterruptIn select(D8);
+// InterruptIn up(D9);
+// uLCD_4DGL uLCD(D1, D0, D2);
+// EventQueue queue(32 * EVENTS_EVENT_SIZE);
+// Thread t;
+
+// int i;
+
+// void add()
+// {   
+//     i=i+1;
+//     uLCD.printf("%d",i);
+// }
+
+// int main() 
+// {   
+//     i=3;
+//     uLCD.text_width(4); //4X size text
+//     uLCD.text_height(4);
+//     uLCD.color(RED);
+//     uLCD.printf("%d",i);
+//     t.start(callback(&queue, &EventQueue::dispatch_forever));
+//     up.rise(queue.event(add));      
+//     //down.rise(&minus); 
+// }
